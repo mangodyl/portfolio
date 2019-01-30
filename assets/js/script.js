12c6f5f06302f8db5aab34e51a082318275a8f25
mango dyl <dylanbonds@hotmail.com>
2019-01-30 01:00:01 -0600

    carousel fixes, media queries

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,19 @@ $(function() {
     });
 
 
-    $("")
+    $("#intro-btn").on("click", function () {
+
+        event.preventDefault();
+
+        var hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function () {
+            window.location.hash = hash;
+        })
+
+
+    });
     
 });
