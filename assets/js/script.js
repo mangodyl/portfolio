f70e61bf8d7f231f331bc08a582392c190261fdf
mango dyl <dylanbonds@hotmail.com>
2019-01-31 17:50:21 -0600

    offset added

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,7 @@ $(function() {
             var hash = this.hash;
 
             $('html, body').animate({
-                scrollTop: $(hash).offset().top
+                scrollTop: $(hash).offset().top - 50
               }, 800, function(){
                 window.location.hash = hash;
             });
@@ -37,7 +37,7 @@ $(function() {
         var hash = this.hash;
 
         $('html, body').animate({
-            scrollTop: $(hash).offset().top
+            scrollTop: $(hash).offset().top -50
         }, 800, function () {
             window.location.hash = hash;
         });
